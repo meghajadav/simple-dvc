7b8ed18ae330689b2946a49c17e69f50eea47366
meghajadav <meghasaraiya@gmail.com>
2022-02-25 08:10:59 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+conda create -n wineq1 python=3.7 -y
+
+conda activate wineq1
+
+pip install -r requirements.txt
---
+++ template.py
@@ -0,0 +1,24 @@
+import os
+
+dirs = ['src',
+'data_given',
+os.path.join('data', 'raw'),
+os.path.join('data', 'processed'),
+'notebooks',
+'save_models'
+]
+
+for dir in dirs:
+    os.makedirs(dir, exist_ok=True)
+    with open(os.path.join(dir, '.gitkeep'),'w') as f:
+        pass
+
+files = ['params.yaml',
+        'dvc.yaml',
+        '.gitignore',
+        os.path.join('src', '__init__.py')
+        ]
+
+for file in files: 
+    with open(file, 'w') as f:
+        pass
