c65059882829a19c36b5bf51f9712418ec2ea4c1
meghajadav <meghasaraiya@gmail.com>
2022-03-01 14:45:22 -0500

    tox

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    description="it's a wine package",
+    author='Megha',
+    packages=find_packages(),
+    license='MIT'
+)
---
+++ tests/test_config.py
@@ -0,0 +1,4 @@
+def test():
+    a = 2 
+    b = 2
+    assert a==b 
